273bf788d0776aafc4fec32ff534045830324a18
erikokres <erikokresna@gmail.com>
2022-11-19 12:58:49 +0700

    Add Key Puzzle System

--- The Escape of Timun Mas/Assets/Scripts/Puzzle Manager/DoorPuzzle.cs
+++ The Escape of Timun Mas/Assets/Scripts/Puzzle Manager/DoorPuzzle.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class DoorPuzzle : MonoBehaviour
 {
    public GameObject doorObject;
+   KeyPuzzle _keyPuzz;
    
+   private void Start() {
+      _keyPuzz = FindObjectOfType<KeyPuzzle>();
+   }
+
    public void OpenDoor()
    {
       doorObject.SetActive(false);
    }
+
 }
---
+++ The Escape of Timun Mas/Assets/Scripts/Puzzle Manager/KeyPuzzle.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPuzzle : MonoBehaviour
+{
+
+    public GameObject Keys;
+    public GameObject Door;
+
+    private bool _getKey;
+
+
+
+    private void OnTriggerStay(Collider other) {
+
+
+        if(other.CompareTag("Keys")&&Input.GetKey(KeyCode.E)){
+            Keys.SetActive(false);
+            _getKey = true;
+        }
+
+        if(other.CompareTag("Interactable") && _getKey == true){
+            Door.SetActive(false);
+        }
+    }
+
+}
