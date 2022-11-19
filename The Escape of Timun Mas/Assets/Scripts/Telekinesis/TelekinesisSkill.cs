41ae5a19779f7db857e31ce3321be26146591f9f
Dionovan Ramadhani <80899280+kizaruu27@users.noreply.github.com>
2022-11-19 09:24:06 +0700

    create telekinesis skill

---
+++ The Escape of Timun Mas/Assets/Scripts/Telekinesis/TelekinesisSkill.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ray = Unity.Physics.Ray;
+
+public class TelekinesisSkill : MonoBehaviour
+{
+    public float transitionSpeed = 5;
+    public Transform targetTransform;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Interactable")
+        {
+            if (Input.GetMouseButton(0))
+            {
+                other.transform.position = Vector3.MoveTowards(other.transform.position, targetTransform.position, transitionSpeed * Time.deltaTime);
+                other.transform.GetComponent<Rigidbody>().useGravity = false;
+                other.transform.GetComponent<BoxCollider>().isTrigger = true;
+            }
+            else
+            {
+                other.transform.GetComponent<Rigidbody>().useGravity = true;
+                other.transform.GetComponent<BoxCollider>().isTrigger = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Interactable")
+        {
+            other.transform.GetComponent<Rigidbody>().useGravity = true;
+            other.transform.GetComponent<BoxCollider>().isTrigger = false;
+        }
+    }
+}
